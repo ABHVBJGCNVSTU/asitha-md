19ecc2ac866ae7ec66b58351b1ae77f024d3ca1d
amir <118857034+ABHVBJGCNVSTU@users.noreply.github.com>
2025-02-05 17:52:47 +0500

    Update settings.js
    Signed-off-by: amir <118857034+ABHVBJGCNVSTU@users.noreply.github.com>

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=L2xCBKZD#NhfPy8A1yYqEfBmsBUUzFEDWC17Bn9KtreaCpAgb4iQ' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=2iIHXTqa#PzcleuKRthZRnnvNlu9gal_VrOAIwl_jjmksYGi3xi0' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SUDO: process.env.SUDO === undefined ? '923114397148,923114397148' : process.env.SUDO,
